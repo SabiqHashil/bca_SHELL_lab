96bb3d1dbfc14ef598dc8248b294f1e232d07b64
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-01-25 23:43:04 +0530

    4 added

---
+++ aim4.sh
@@ -0,0 +1,9 @@
+# Write a shell script to find the greatest of three numbers
+
+echo "Enter three Integers:" read a b c
+
+if [ $a -gt $b -a $a -gt $c ] then
+echo "$a is Greatest"
+elif [ $b -gt $c -a $b -gt $a ] then
+echo "$b is Greatest" else
+echo "$c is Greatest!" fi

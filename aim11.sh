ec0bdc020119a2f9a9f4f3d82908afc8c33191d6
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-02-05 21:29:50 +0530

    11 added

---
+++ aim11.sh
@@ -0,0 +1,15 @@
+# Write a shell script to reverse a string and check whether a given string is palindrome or not.
+
+echo Enter the string 
+read s
+echo $s > temp 
+rvs="$(rev temp)" 
+if [ $s = $rvs ] 
+then
+    echo " it is a Palindrome"
+else 
+    echo " it is not a Palindrome"
+fi
+ 
+
+

19be702add6514553a12d9857478690c2b8ec052
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-02-20 21:41:12 +0530

    15 added

---
+++ aim15.sh
@@ -0,0 +1,14 @@
+# Write a shell script find the factorial of a given number
+
+echo "Enter a number" 
+read num
+
+fact=1
+
+while [ $num -gt 1 ] 
+do
+fact=$((fact * num)) 
+num=$((num - 1)) 
+done
+
+echo $fact

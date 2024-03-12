be4c2b132aa4aaa19fc2019f22a03c945270ff0f
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-03-12 20:52:51 +0530

    updated

--- aim7.sh
+++ aim7.sh
@@ -1,17 +1,17 @@
-# Write a shell script to find reverse of a number
+# 7. Write a shell script to find reverse of a number 
 
-echo "Enter a Number:"
-read a
+ 
 
-rev=0
-sd=0
-or=$a
-
-while [ $a -gt 0 ]; do
-    sd=$(expr $a % 10)
-    temp=$(expr $rev \* 10)
-    rev=$(expr $temp + $sd)
-    a=$(expr $a / 10)
-done
-
-echo "Reverse of $or is $rev"
+echo "Enter a Number:" 
+      read a 
+rev=0 
+sd=0 
+or=$a 
+while [ $a -gt 0 ] 
+      do 
+           sd=`expr $a % 10` 
+           temp=`expr $rev \* 10` 
+           rev=`expr $temp + $sd` 
+           a=`expr $a / 10` 
+      done 
+echo "Reverse of $or is $rev" 
--- aim9.sh
+++ aim9.sh
@@ -1,17 +1,22 @@
-# Write a shell script to find n Fibonacci numbers
+# 9. Write a shell script to find n Fibonacci numbers 
 
-echo "How many numbers do you want of Fibonacci series ?"
-read total
-x=0
-y=1
-i=2
-echo "Fibonacci Series up to $total terms :: "
-echo "$x"
-echo "$y"
-while [ $i -lt $total ]; do
-    i=$(expr $i + 1)
-    z=$(expr $x + $y)
-    echo "$z"
-    x=$y
-    y=$z
-done
+ 
+
+ 
+
+echo "How many numbers do you want of Fibonacci series ?" 
+     read total 
+x=0 
+y=1 
+i=2 
+echo "Fibonacci Series up to $total terms :: " 
+echo "$x" 
+echo "$y" 
+while [ $i -lt $total ] 
+      do 
+        i=`expr $i + 1 ` 
+        z=`expr $x + $y ` 
+        echo "$z" 
+        x=$y 
+        y=$z 
+      done 
